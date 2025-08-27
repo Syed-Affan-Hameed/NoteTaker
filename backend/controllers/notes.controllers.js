e0a9c70487d0c30dfe382ddf97f4b99e87578027
Syed Affan Hameed <syedaffanhameed@gmail.com>
2025-08-28 00:10:45 +0530

    feat: implement notes API with CRUD operations and routing

---
+++ backend/controllers/notes.controllers.js
@@ -0,0 +1,17 @@
+
+
+export const getAllNotes=(req, res) => {
+    res.status(200).send('Notes API Endpoint:  you have 5 notes');
+};
+
+export const addNote =(req, res) => {
+    res.status(201).send(`Note: ${req.body.noteText} added successfully`);
+};
+
+export const updateNote =(req, res) => {
+    res.status(200).json({ message: `Note with ID ${req.params.id} updated successfully` });
+}
+
+export const deleteNote =(req,res)=>{
+    res.status(200).json({message:`Note with ID ${req.params.id} is successfully deleted`});
+}
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,35 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import notesRouter from './routes/notes.routes.js';
 
 const app=express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5005;
+app.use(express.json());
+app.use('/api/notes',notesRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('Hello, World!');
+// });
+
+// app.get('/api/notes',(req,res)=>{
+//     res.status(200).send('Notes API Endpoint:  you have 5 notes');
+// })
+
+// app.post('/api/addnote',(req,res)=>{
+//     res.status(201).send('Note added successfully');
+// }   );
+
+// app.put('/api/updatenote/:id',(req,res)=>{
+//     res.status(200).json({ message: `Note with ID ${req.params.id} updated successfully` });
+// })
+// app.delete('/api/deletenote/:id',(req,res)=>{
+//     res.status(200).json({ message: `Note with ID ${req.params.id} deleted successfully` });
+// });
