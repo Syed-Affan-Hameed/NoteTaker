c7367a5b3f8532bed8d9927055335e7218e6d704
Syed Affan Hameed <syedaffanhameed@gmail.com>
2025-09-07 02:53:56 +0530

    feat: implement update and delete functionality in notes controller with error handling

--- backend/src/controllers/notes.controllers.js
+++ backend/src/controllers/notes.controllers.js
@@ -31,14 +31,44 @@ export const addNote = async (req, res) => {
   // res.status(201).send(`Note: ${req.body.noteText} added successfully`);
 };
 
-export const updateNote = (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Note with ID ${req.params.id} updated successfully` });
+export const updateNote =async (req, res) => {
+
+    //We need to specify which note we are going to update by its id in the database
+    // we will get the required id from the query params
+    try {
+         const newTitle = req.body.title;
+    const newContent =req.body.content;
+    const idOftheNoteToBeUpdated = req.params.id;
+    const noteTobeUpdated = await NoteModel.findByIdAndUpdate(idOftheNoteToBeUpdated,{title:newTitle,content:newContent},{new:true});
+
+    if(noteTobeUpdated){
+        res.status(200).json({message:"Note Updated successfully", Note:noteTobeUpdated});
+    }
+    else{
+        res.status(404).json({message:"404: Not Found"})
+    }   
+
+    } catch (error) {
+        console.log("Error updating the note",error);
+         res.status(500).json({message:"500: Internal server error"});
+    }
 };
 
-export const deleteNote = (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Note with ID ${req.params.id} is successfully deleted` });
+export const deleteNote = async(req, res) => {
+    try {
+        const noteToBeDeletedId= req.params.id;
+        const deletedNote = await NoteModel.findByIdAndDelete(noteToBeDeletedId);
+
+        if(!deletedNote){
+        res.status(404).json({message:"404 Not Found"});
+        }
+        res.status(200).json({message:"Note Deleted Successfully",Note:deletedNote});
+
+
+    } catch (error) {
+          console.log("Error updating the note",error);
+         res.status(500).json({message:"500: Internal server error"});
+
+    }
+
 };
